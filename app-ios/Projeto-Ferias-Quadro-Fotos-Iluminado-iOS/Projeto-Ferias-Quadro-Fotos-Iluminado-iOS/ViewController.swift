b6ffe635990bc9c80129680a936632db41f8f669
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-06-27 23:11:56 -0300

    Adicionado projeto em Swift

---
+++ app-ios/Projeto-Ferias-Quadro-Fotos-Iluminado-iOS/Projeto-Ferias-Quadro-Fotos-Iluminado-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projeto-Ferias-Quadro-Fotos-Iluminado-iOS
+//
+//  Created by Humberto Vieira de Castro on 6/27/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
